a05f3c66f1b601a484410e7e15676d74bb71ab27
optimizion <optimizion@gmail.com>
2020-12-03 19:28:12 +0900

    TABLE : getter & setter

--- cpp-console-database/ManagaDatabase/ManagaDatabase.cpp
+++ cpp-console-database/ManagaDatabase/ManagaDatabase.cpp
@@ -4,15 +4,6 @@
 
 using namespace std;
 
-enum CONSOLE
-{
-	MAIN,
-	MANAGE_TABLE,
-	SELECT_TABLE,
-	MANAGE_TABLE_DATA,
-	EXIT
-};
-
 class ManageDatabase
 {
 private:
@@ -26,19 +17,144 @@ public:
 		COURSE
 	};
 	//create table
-	void createTable();
+	void createTable() {};
 	//delete table
-	void deleteTable();
+	void deleteTable()
+	{
+		// if(!테이블 존재 여부?) {"table doesn't exist"} else {해당 테이블 찾아서 파일 삭제, "table is successfully deleted"}
+	};
 	//테이블 존재 여부 확인
-	bool hasTable(ManageDatabase::TABLE table);
+	bool hasTable(ManageDatabase::TABLE table) {};
 	//테이블을 모아둘 폴더 생성
 	void makeDirectory();
+};
+
+class Console
+{
+public:
+	enum TYPE
+	{
+		MAIN,
+		MANAGE_TABLE,
+		SELECT_TABLE,
+		MANAGE_TABLE_DATA,
+		EXIT
+	};
+
+	void printConsole(TYPE console)
+	{
+		switch (console)
+		{
+		case Console::MAIN:
+			Console::printMain();
+			break;
+		case Console::MANAGE_TABLE:
+			Console::printManageTable();
+			break;
+		case Console::SELECT_TABLE:
+			Console::printSelectTable();
+			break;
+		case Console::MANAGE_TABLE_DATA:
+			Console::printManageTableData();
+			break;
+		case TYPE::EXIT:
+			Console::printExit();
+		default:
+			break;
+		}
+	}
+	void clearConsole()
+	{
+		system("cls");
+	}
+	void printMain()
+	{
+		cout << ",-------------------------------------------.\n";
+		cout << "|                                           |\n";
+		cout << "|       ,---------------------------.       |\n";
+		cout << "|       |                           |       |\n";
+		cout << "|       |      Manage Database      |       |\n";
+		cout << "|       |                           |       |\n";
+		cout << "|       `---------------------------'       |\n";
+		cout << "|                                           |\n";
+		cout << "|              1. Manage Table              |\n";
+		cout << "|              2. Manage Table Data         |\n";
+		cout << "|              3. Exit                      |\n";
+		cout << "|                                           |\n";
+		cout << "`-------------------------------------------'\n";
+	}
+	void printManageTable()
+	{
+		cout << ",-------------------------------------------.\n";
+		cout << "|                                           |\n";
+		cout << "|       ,---------------------------.       |\n";
+		cout << "|       |                           |       |\n";
+		cout << "|       |        Manage Table       |       |\n";
+		cout << "|       |                           |       |\n";
+		cout << "|       `---------------------------'       |\n";
+		cout << "|                                           |\n";
+		cout << "|              1. Create Table              |\n";
+		cout << "|              2. Delete Table              |\n";
+		cout << "|              3. Go Back                   |\n";
+		cout << "|                                           |\n";
+		cout << "`-------------------------------------------'\n";
+	}
+	void printManageTableData()
+	{
+		cout << ",-------------------------------------------.\n";
+		cout << "|                                           |\n";
+		cout << "|       ,---------------------------.       |\n";
+		cout << "|       |                           |       |\n";
+		cout << "|       |     Manage Table Data     |       |\n";
+		cout << "|       |                           |       |\n";
+		cout << "|       `---------------------------'       |\n";
+		cout << "|                                           |\n";
+		cout << "|              1. Insert Data               |\n";
+		cout << "|              2. Update Data               |\n";
+		cout << "|              3. Delete Data               |\n";
+		cout << "|              4. Go Back                   |\n";
+		cout << "|                                           |\n";
+		cout << "`-------------------------------------------'\n";
+	}
+	void printSelectTable()
+	{
+		cout << ",-------------------------------------------.\n";
+		cout << "|                                           |\n";
+		cout << "|       ,---------------------------.       |\n";
+		cout << "|       |                           |       |\n";
+		cout << "|       |        Select Table       |       |\n";
+		cout << "|       |                           |       |\n";
+		cout << "|       `---------------------------'       |\n";
+		cout << "|                                           |\n";
+		cout << "|              1. Student                   |\n";
+		cout << "|              2. Subject                   |\n";
+		cout << "|              3. Course                    |\n";
+		cout << "|              4. Go Back                   |\n";
+		cout << "|                                           |\n";
+		cout << "`-------------------------------------------'\n";
+	}
+	void printExit()
+	{
+		cout << ",-------------------------------------------.\n";
+		cout << "|                                           |\n";
+		cout << "|       ,---------------------------.       |\n";
+		cout << "|       |                           |       |\n";
+		cout << "|       |            Exit           |       |\n";
+		cout << "|       |                           |       |\n";
+		cout << "|       |                           |       |\n";
+		cout << "|       |          Good Bye         |       |\n";
+		cout << "|       |                           |       |\n";
+		cout << "|       `---------------------------'       |\n";
+		cout << "|                                           |\n";
+		cout << "`-------------------------------------------'\n";
+	}
 };
 
 int main()
 {
 	//시작 화면 출력
-	printConsole(CONSOLE::MAIN);
+	Console console;
+	console.printConsole(Console::TYPE::MAIN);
 
 	int user_input;
 	cin >> user_input;
@@ -49,18 +165,20 @@ int main()
 		if (user_input == 1)
 		{
 			// clear console, show manage table console
+			console.clearConsole();
+			console.printConsole(Console::TYPE::MANAGE_TABLE);
 			while (true)
 			{
 				// create table
-				if ()
+				if (true)
 				{
 				}
 				// delete table
-				else if ()
+				else if (true)
 				{
 				}
 				// go back
-				else if ()
+				else if (true)
 				{
 				}
 				// wrong input
@@ -73,18 +191,20 @@ int main()
 		else if (user_input == 2)
 		{
 			// clear console, show manage select table console
+			console.clearConsole();
+			console.printConsole(Console::TYPE::MANAGE_TABLE_DATA);
 			while (true)
 			{
 				// student table
-				if ()
+				if (true)
 				{
 				}
 				// subject table
-				else if ()
+				else if (true)
 				{
 				}
 				// course table
-				else if ()
+				else if (true)
 				{
 				}
 				// wrong input
@@ -97,6 +217,8 @@ int main()
 		else if (user_input == 3)
 		{
 			// clear console, show exit console
+			console.clearConsole();
+			console.printConsole(Console::TYPE::EXIT);
 		}
 		//wrong input
 		else
@@ -105,194 +227,12 @@ int main()
 	}
 }
 
-// Console Method
-void printConsole(CONSOLE console)
-{
-	switch (console)
-	{
-	case MAIN:
-		printMain();
-		break;
-	case MANAGE_TABLE:
-		printManageTable();
-		break;
-	case SELECT_TABLE:
-		printSelectTable();
-	case MANAGE_TABLE_DATA:
-		printManageTableData();
-		break;
-	case EXIT:
-		printExit();
-	default:
-		break;
-	}
-};
-void clearConsole()
-{
-	system("cls");
-}
-void printMain()
-{
-	cout << ",-------------------------------------------.\n";
-	cout << "|                                           |\n";
-	cout << "|       ,---------------------------.       |\n";
-	cout << "|       |                           |       |\n";
-	cout << "|       |      Manage Database      |       |\n";
-	cout << "|       |                           |       |\n";
-	cout << "|       `---------------------------'       |\n";
-	cout << "|                                           |\n";
-	cout << "|              1. Manage Table              |\n";
-	cout << "|              2. Manage Table Data         |\n";
-	cout << "|              3. Exit                      |\n";
-	cout << "|                                           |\n";
-	cout << "`-------------------------------------------'\n";
-}
-void printManageTable()
-{
-	cout << ",-------------------------------------------.\n";
-	cout << "|                                           |\n";
-	cout << "|       ,---------------------------.       |\n";
-	cout << "|       |                           |       |\n";
-	cout << "|       |        Manage Table       |       |\n";
-	cout << "|       |                           |       |\n";
-	cout << "|       `---------------------------'       |\n";
-	cout << "|                                           |\n";
-	cout << "|              1. Create Table              |\n";
-	cout << "|              2. Delete Table              |\n";
-	cout << "|              3. Go Back                   |\n";
-	cout << "|                                           |\n";
-	cout << "`-------------------------------------------'\n";
-}
-void printManageTableData()
-{
-	cout << ",-------------------------------------------.\n";
-	cout << "|                                           |\n";
-	cout << "|       ,---------------------------.       |\n";
-	cout << "|       |                           |       |\n";
-	cout << "|       |     Manage Table Data     |       |\n";
-	cout << "|       |                           |       |\n";
-	cout << "|       `---------------------------'       |\n";
-	cout << "|                                           |\n";
-	cout << "|              1. Insert Data               |\n";
-	cout << "|              2. Update Data               |\n";
-	cout << "|              3. Delete Data               |\n";
-	cout << "|              4. Go Back                   |\n";
-	cout << "|                                           |\n";
-	cout << "`-------------------------------------------'\n";
-}
-void printSelectTable()
-{
-	cout << ",-------------------------------------------.\n";
-	cout << "|                                           |\n";
-	cout << "|       ,---------------------------.       |\n";
-	cout << "|       |                           |       |\n";
-	cout << "|       |        Select Table       |       |\n";
-	cout << "|       |                           |       |\n";
-	cout << "|       `---------------------------'       |\n";
-	cout << "|                                           |\n";
-	cout << "|              1. Student                   |\n";
-	cout << "|              2. Subject                   |\n";
-	cout << "|              3. Course                    |\n";
-	cout << "|              4. Go Back                   |\n";
-	cout << "|                                           |\n";
-	cout << "`-------------------------------------------'\n";
-}
-void printExit()
-{
-	cout << ",-------------------------------------------.\n";
-	cout << "|                                           |\n";
-	cout << "|       ,---------------------------.       |\n";
-	cout << "|       |                           |       |\n";
-	cout << "|       |            Exit           |       |\n";
-	cout << "|       |                           |       |\n";
-	cout << "|       |                           |       |\n";
-	cout << "|       |          Good Bye         |       |\n";
-	cout << "|       |                           |       |\n";
-	cout << "|       `---------------------------'       |\n";
-	cout << "|                                           |\n";
-	cout << "`-------------------------------------------'\n";
-}
+
+
 
 // Manage Table
-void ManageDatabase::createTable()
-{
-}
-void ManageDatabase::deleteTable()
-{
-	// if(!테이블 존재 여부?) {"table doesn't exist"} else {해당 테이블 찾아서 파일 삭제, "table is successfully deleted"}
-}
-bool ManageDatabase::hasTable(TABLE table)
-{
-	// 테이블이 있는지 확인하는 코드
-}
 
-// 다른 파일로 뺄 부분
-void userInsert()
-{
-	clearConsole();
-	ifstream input;
-	input.open("database.txt");//열람할 파일 이름 적는 공간
-	ofstream writefile;
-	writefile.open("database.txt"); //쓰기위한 파일 오픈
-	int i = 0;
-	if (input.fail())
-	{
-		cout << "**파일이 존재하지 않습니다.**" << endl;
-		cout << "**파일을 생성합니다.**" << endl;
-		ofstream writefile; //파일 위치는 소스코드 파일
-		writefile.open("database.txt");
-	}
 
-	string write_line;
-	cout << endl << endl;
-	cout << "데이터를 5번 입력하면 종료안내가 나옵니다." << endl << endl;
-	cout << ",-----------------------------------------------------------------------------------------." << endl;
-	cout << "|순번| 과목코드 | 과목명 | 이수구분 | 교수명 | 학점 | 시간표 | 상대평가 유형 | 수업계획서 |" << endl;
-	cout << "'-----------------------------------------------------------------------------------------'" << endl;
-	char key;
-	while (i < User_MAX)
-	{
-		cin >> write_line; //write_line 문자 입력받기
-		i++;
-		writefile << write_line << endl; //입력받은 문자를 파일에 넣기
-		if (i % 5 == 0)
-		{
-			char ch;
-			cout << "입력을 종료하시겠습니까?(Y/N):";
-			cin >> ch;
-			if (ch == 'Y' || ch == 'y')
-			{
-				cout << "입력을 종료합니다" << endl << endl;
-				cout << "1. RE Insert 2. Show Data 3. Delete Data 4. Back " << endl;
-				break;
-			}
-			else if (ch == 'N' || ch == 'n')
-			{
-
-			}
-		}
-
-		/*key = _getch();
-		if (key == ESC) {
-		cout << "입력을 종료합니다" << endl << endl;
-		cout << "1. RE Insert 2. Show Data 3. Delete Data 4. Back " << endl;
-		break;
-		}*/
-
-	}
-
-	input.close();
-
-}
-void showUser()
-{
-	cout << ",=========================================================================================.\n";
-	cout << "|순번| 과목코드 | 과목명 | 이수구분 | 교수명 | 학점 | 시간표 | 상대평가 유형 | 수업계획서  |\n";
-	cout << "`========================================================================================='\n";
-}
-void userDel() {}
-
-// 파일로 저장하는 부분
 /*
 
 	Printmain();
---
+++ cpp-console-database/ManagaDatabase/Table.cpp
@@ -0,0 +1,148 @@
+#include <string>
+using namespace std;
+
+class Student
+{
+private:
+	// NOT NULL
+	string studentId; // 학번 -> PK (0 < length <= 10자리 이하)
+	string name; // 이름 (0 < length)
+	char sex; // 성별 (default m)
+	short grade; // 학년 (default 1)
+	short age; // 나이 (default 20)
+
+	// NULLABLE
+	string email; // 이메일 (must include @)
+	string phoneNum; // 연락처 (not include - )
+	string address; // 주소
+public:
+	Student(string studentId, string name, char sex = 'm', short grade = 1, short age = 20, string email = NULL, string phoneNum = NULL, string address = NULL)
+	{
+		this->studentId = studentId;
+		this->name = name;
+		this->sex = sex;
+		this->grade = grade;
+		this->age = age;
+		this->email = email;
+		this->phoneNum = phoneNum;
+		this->address = address;
+	};
+	// getter
+	string getStudentId() { return this->studentId; }
+	string getName() { return this->name; }
+	char getSex() { return this->sex; }
+	short getGrade() { return this->grade; }
+	short getAge() { return this->age; }
+	string getEmail() { return this->email; }
+	string getPhoneNum() { return this->phoneNum; }
+	string getAddress() { return this->address; }
+
+	// setter
+	void setStudentId(string id) { this->studentId = id; }
+	void setName(string name) { this->name = name; }
+	void setSex(char sex) { this->sex = sex; }
+	void setGrade(short grade) { this->grade = grade; }
+	void setAge(short age) { this->age = age; }
+	void setEmail(string email) { this->email = email; }
+	void setPhoneNum(string phoneNum) { this->phoneNum = phoneNum; }
+	void setAddress(string address) { this->address = address; }
+
+};
+
+class Subject
+{
+private:
+	// NOT NULL
+	string classCode;	//과목코드 -> PK
+	string className;	//과목명
+
+	// NULLABLE
+	string professor;	//교수 
+	int grade;			//학점 
+	string timePlan;	//시간표 -> '화(7,8,9)' 
+	string classified;	//이수구분(전공,교양) 
+	string evalType;	//상대평가유형
+public:
+	Subject(string classCode, string className, string professor, int grade, string timePlan, string classified, string evalType)
+	{
+		this->classCode = classCode;
+		this->className = className;
+		this->professor = professor;
+		this->grade = grade;
+		this->timePlan = timePlan;
+		this->classified = classified;
+		this->evalType = evalType;
+	};
+	//getter
+	string getClassCode() { return classCode; }
+	string getClassName() { return className; }
+	string getProfessor() { return professor; }
+	int getGrade() { return grade; }
+	string getTimePlan() { return timePlan; }
+	string getClassified() { return classified; }
+	string getEvalType() { return evalType; }
+
+	//setter
+	void setClassCode(string classCode) { this->classCode = classCode; }
+	void setClassName(string className) { this->className = className; }
+	void setProfessor(string professor) { this->professor = professor; }
+	void setGrade(int grade) { this->grade = grade; }
+	void setTimePlan(string timePlan) { this->timePlan = timePlan; }
+	void setClassified(string classified) { this->classified = classified; }
+	void setEvalType(string evalType) { this->evalType = evalType; }
+};
+
+class Course
+{
+private:
+	// NOT NULL
+	string studentId;     //학번 -> PK / FK
+	string classCode;     //과목코드 -> PK / FK
+	string className;     //과목명
+	string professor;       //교수
+
+	// NULLABLE
+	string timePlan;          //시간표
+	string location;           //장소
+
+public:
+	Course(string studentId, string classCode, string className, string professor, string timePlan, string location)
+	{
+		this->studentId = studentId;
+		this->classCode = classCode;
+		this->className = className;
+		this->professor = professor;
+		this->timePlan = timePlan;
+		this->location = location;
+	}
+	//getter
+	string getStudentId() { return studentId; }
+	string getClassCode() { return classCode; }
+	string getClassName() { return className; }
+	string getProfessor() { return professor; }
+	string getTimePlan() { return timePlan; }
+	string getLocation() { return location; }
+	//setter
+	void setStudentId(string studentId) { this->studentId = studentId; }
+	void setClassCode(string classCode) { this->classCode = classCode; }
+	void setClassName(string className) { this->className = className; }
+	void setProfessor(string professor) { this->professor = professor; }
+	void setTimePlan(string timePlan) { this->timePlan = timePlan; }
+	void setLocation(string location) { this->location = location; }
+};
+
+class LoginInfo
+{
+	// NOT NULL
+	string studentId;
+	string password;
+
+public:
+	LoginInfo(string studentId, string password = "1234") { this->studentId = studentId; this->password = password; }
+	//getter
+	string getStudentId() { return studentId; }
+	string getPassword() { return password; }
+	//setter
+	void setStudentId(string studentId) { this->studentId = studentId; }
+	void setPassword(string password) { this->password = password; }
+};
